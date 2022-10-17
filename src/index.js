eaf078c0f933ed5ba9a34904c7ec9ce4926de6c7
Bill <bill@Bills-MacBook-Pro.local>
2022-10-17 10:19:39 -0700

    added in our addToCart feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,11 +11,11 @@ import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // const rootElement = document.getElementById('root');
 
-render(
+  // render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
@@ -26,8 +26,8 @@ render(
         </ProductProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
+  // rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
