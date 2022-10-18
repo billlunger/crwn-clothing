08c75fa173eaa2a9d6d75a136af1e1aca00b37a2
Bill <bill@Bills-MacBook-Pro.local>
2022-10-18 14:23:32 -0700

    started applying styled components

--- src/components/category-directory/category-directory.component.jsx
+++ src/components/category-directory/category-directory.component.jsx
@@ -1,12 +1,12 @@
 import './category-directory.styles.scss';
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 let directory = require('./category-directory.json');
 
 const CategoryDirectory = () => {
   return (
     <div className='categories-container'>
       {directory.map(category => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
